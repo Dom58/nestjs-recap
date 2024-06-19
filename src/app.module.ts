feba37fe656b8f1e5a79837bda17287b7025b009
Dom58 <dndahimana58@gmail.com>
2024-06-19 17:05:53 +0200

    Updating .env variables

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,20 +1,26 @@
 import { Module } from '@nestjs/common';
 import { SequelizeModule } from '@nestjs/sequelize';
+import * as dotenv from 'dotenv';
 import { UserModule } from './user/user.module';
 import { User } from './database/models/user.entity';
 
+dotenv.config();
+
+const environment = 'DEVELOPMENT'
 @Module({
   imports: [
     SequelizeModule.forRoot({
       dialect: 'postgres',
-      host: 'localhost',
-      port: 5432,
-      username: 'postgres',
-      password: 'domdom',
-      database: 'leads_management',
+      timezone: process.env.APP_TIMEZONE as string,
+      ssl: process.env[`DB_${environment}_SSL`] === 'TRUE',
+      host: process.env[`DB_${environment}_HOST`] as string,
+      username: process.env[`DB_${environment}_USER`] as string,
+      database: process.env[`DB_${environment}_NAME`] as string,
+      password: process.env[`DB_${environment}_PASSWORD`] as string,
+      port: parseInt(process.env[`DB_${environment}_PORT`] as string, 10),
       models: [User],
       autoLoadModels: true,
-      synchronize: true,
+      synchronize: true
     }),
     UserModule,
   ],
